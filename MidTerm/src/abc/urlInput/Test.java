a512a69a9f5cdf19df04d900ada35bd3d00faff8
kenny132652 <73508417+kenny132652@users.noreply.github.com>
2024-09-18 22:50:15 +0800

    Add files via upload

--- MidTerm/src/abc/urlInput/Test.java
+++ MidTerm/src/abc/urlInput/Test.java
@@ -7,10 +7,14 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import abc.bean.Liquor;
+import abc.util.ConnUtil;
 
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Test {
 
@@ -25,9 +29,23 @@ public class Test {
             // for (Liquor lb : liquors) {
             //     System.out.println(lb.toString());
             // }
-
-            
-
+            String sql = "insert into Liquor values(?,?,?,?,?,?,?)";
+            try (Connection conn = ConnUtil.getconnection("TTL")){
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                for (Liquor l : liquors) {
+                    pstmt.setString(1, l.get產品類別());
+                    pstmt.setString(2, l.get產品代號());
+                    pstmt.setString(3, l.get品名());
+                    pstmt.setString(4, l.get包裝());
+                    pstmt.setString(5, l.get單位());
+                    pstmt.setString(6, l.get建議售價());
+                    pstmt.setString(7, l.get備註());
+                    int updatecount=pstmt.executeUpdate();
+                }
+                System.out.printf("已新增%d筆資料\n",updatecount);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
         } catch (Exception e) {
         e.printStackTrace();
--- MidTerm/src/abc/urlInput/UrlInput.java
+++ MidTerm/src/abc/urlInput/UrlInput.java
@@ -3,12 +3,16 @@ package abc.urlInput;
 import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 
 import com.opencsv.*;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import abc.bean.Liquor;
+import abc.util.ConnUtil;
 
 public class UrlInput {
     public static void main(String[] args) {
@@ -17,21 +21,31 @@ public class UrlInput {
             InputStream openStream = url.openStream();
             InputStreamReader isr = new InputStreamReader(openStream,Charset.forName("big5"));
             BufferedReader br = new BufferedReader(isr);
-            CSVReader reader = new CSVReaderBuilder(isr).build();
+            CSVReader reader = new CSVReader(br);
+            reader.readNext();
+            List<Liquor> liquors = new CsvToBeanBuilder(reader).withType(Liquor.class).build().parse();
 
-            // FileReader fr = new FileReader("C:/Users/User/Desktop/code/midtermreport/MidTerm/臺灣菸酒股份有限公司產品價目表 .csv",Charset.forName("big5"));
-            // CSVReader reader2 = new CSVReaderBuilder(fr).build();
-            // List<String[]> myEntries = reader2.readAll();
-            // List<LiquorBean> beans = new CsvToBeanBuilder(fr).withType(LiquorBean.class).build().parse();
+  
+            // for (Liquor lb : liquors) {
+            //     System.out.println(lb.toString());
+            // }
             
-            
-            // System.out.println(Arrays.toString(beans.toArray()));
-
-            String [] nextline;
-            while((nextline = reader.readNext())!= null){
-                for(String string : nextline){
-                    System.out.println(string);
-                }
+            String sql = "insert into Liquor values(?,?,?,?,?,?,?)";
+            try (Connection conn = ConnUtil.getconnection("TTL")){
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                for (Liquor l : liquors) {
+                    pstmt.setString(1, l.get產品類別());
+                    pstmt.setString(2, l.get產品代號());
+                    pstmt.setString(3, l.get品名());
+                    pstmt.setString(4, l.get包裝());
+                    pstmt.setString(5, l.get單位());
+                    pstmt.setString(6, l.get建議售價());
+                    pstmt.setString(7, l.get備註());
+                    int updatecount=pstmt.executeUpdate();
+                } 
+                System.out.printf("已新增%d筆資料\n",updatecount);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
             
         } catch (Exception e) {
